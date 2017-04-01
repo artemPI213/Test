76f122073e3b8e6cbc4e5dea7594d8a379d9a770
Admin <admin@gmail.com>
2017-04-01 12:14:48 +0500

    Deleting

--- art/src/art/hfjfgj.java
+++ art/src/art/hfjfgj.java
@@ -30,13 +30,6 @@ public class hfjfgj extends JFrame {
 	/**
 	 * Create the frame.
 	 */
-	public hfjfgj() {
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
-		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
-		setContentPane(contentPane);
-	}
+	
 
 }
